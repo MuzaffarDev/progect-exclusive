89abb16dbd6860f673e670622586b67a86e5efbc
MuzaffaR <abdurahmonovmuzaffar56@gmail.com>
2022-10-20 11:34:38 +0500

    testimonial__card-listdagi flex-wrap off

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
-// $(function () {
-// $('.slider').slick({
+// $('.autoplay-slider').slick({
+//   slidesToShow: 3,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   autoplaySpeed: 2000,
+// });
 
-// })
+// $(".slick-slider").slick({
+//   dots: true,
+//   infinite: true,
+//   slidesToShow: 3,
+//   slidesToScroll: 3,
 // });
